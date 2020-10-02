c9c2d46bc0d841ba1dcf3e9495bf4fd98c757718
haftommit <haftomtesfayusa@gmail.com>
2020-10-02 13:55:19 +0300

    Create Relations between Models

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\Topic;
 use App\Models\User;
 use Illuminate\Database\Seeder;
 
@@ -15,5 +16,9 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         User::factory(20)->create();
+
+        Topic::create(['name' => 'Featured Sites', 'slug' => 'featured']);
+        Topic::create(['name' => 'Useful Links', 'slug' => 'links']);
+        Topic::create(['name' => 'Guides & Tutorials', 'slug' => 'tutorials']);
     }
 }
